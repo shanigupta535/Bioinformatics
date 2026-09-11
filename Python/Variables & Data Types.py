515cf307ede229a8974d1b81db865e06227ecf47
Shani Gupta <shanigupta8282@gmail.com>
2026-09-11 15:20:04 +0530

    add

---
+++ Python/Variables & Data Types.py
@@ -0,0 +1,16 @@
+name = "Shani Gupta" #string
+age = 20.5 #float
+birth = 2026 #integer
+is_student = True #boolean
+
+print(type(name))
+print(type(age))
+print(type(birth))
+print(type(is_student))
+
+# MULTI VARIABLES
+
+name2, age2, branch = "shiva gupta", 22, "CSE"
+a = b = c = 10 #you can do this to assign the same value to multiple variables
+print(name2, age2, branch)
+print(a, b, c)
