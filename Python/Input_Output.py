cb20809f26e3e8e6492db37b54662c92ee4dcc45
Shani Gupta <shanigupta8282@gmail.com>
2026-09-11 16:02:50 +0530

    update

---
+++ Python/Input_Output.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+# STRING INPUT FORMAT
+
+name = input("Enter the name : ")
+age = int(input("Enter the age : "))
+school =  input("Enter the School Name : ")
+
+print(name , "\n" , age , "\n" , school)
+print(type(age))
+
+# NUMERICAL INPUT
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print(a + b)
+
