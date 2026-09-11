74c0322e038d329a371436a21cc5d0bf15a12188
Shani Gupta <shanigupta8282@gmail.com>
2026-09-11 15:27:53 +0530

    add d

--- Python/Variables & Data Types.py
+++ Python/Variables & Data Types.py
@@ -11,6 +11,6 @@ print(type(is_student))
 # MULTI VARIABLES
 
 name2, age2, branch = "shiva gupta", 22, "CSE"
-a = b = c = 10 #you can do this to assign the same value to multiple variables
+a = b = c = d =10 #you can do this to assign the same value to multiple variables
 print(name2, age2, branch)
 print(a, b, c)
